85fccac9c23b507677ab67fd8740d06e990bce85
ikumisato71 <kinoko7971@gmail.com>
2024-07-05 16:41:36 +0900

    Add files via upload
    classListのテスト

---
+++ js/scroll.js
@@ -0,0 +1,24 @@
+// back-btnというidを持つHTML要素を取得し、定数に他代入する
+const backBtn = document.getElementById("back-btn");
+
+// 画面がスクロールされた時にイベント処理を実行
+// window.documentでもdocument.でも動作しますが、ツリーの頂点のwindowに指示する理由ありますか？
+window.addEventListener("scroll", () => {
+  //　画面のスクロール値をpxで取得する
+  const scrollValue = document.scrollingElement.scrollTop;
+  // 画面のスクロール量が300px以上であれば、『TOPに戻る』ボタンを表示
+  if (scrollValue >= 300) {
+    backBtn.style.display = "inline";
+  } else {
+    backBtn.style.display = "none";
+  }
+});
+
+const logo = document.getElementsByClassName("h1");
+document.addEventListener("click", () => {
+  logo.classList.toggle("is-active");
+});
+
+// backBtn.addEventListener("click", () => {
+//   backBtn.classList.toggle("is-active");
+// });
